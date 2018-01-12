d92a1894a8608b4128675b2e1b515f517cbe3f07
Plogging Dev <ploggingdev@gmail.com>
2018-01-12 04:26:11 -0800

    Add option to view chat messages by date

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('topics/<str:topic_name>/chat/archive/', views.ChatArchive.as_view(), name='chat_archive'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
 from .models import Topic, ChatMessage
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+import datetime
+from django.utils import timezone
 
 class IndexView(View):
     template_name = 'mainapp/home_page.html'
@@ -41,3 +45,35 @@ class AboutView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+class ChatArchive(View):
+    template_name = 'mainapp/chat_archive.html'
+
+    def get(self, request, topic_name):
+        try:
+            topic = Topic.objects.get(name=topic_name)
+        except ObjectDoesNotExist:
+            raise Http404("Topic does not exist")
+        first_message = ChatMessage.objects.earliest('created')
+        now = timezone.now()
+        min_date = datetime.datetime(first_message.created.year, first_message.created.month, first_message.created.day, tzinfo=now.tzinfo)
+        given_date = request.GET.get('date', None)
+        error_message = None
+        if given_date == None:
+            given_date = datetime.datetime(now.year, now.month, now.day, tzinfo=now.tzinfo)
+        else:
+            try:
+                #hacky way to set timezone to utc
+                given_date = datetime.datetime.strptime(given_date, "%Y-%m-%d")
+                given_date = datetime.datetime(given_date.year, given_date.month, given_date.day, tzinfo=now.tzinfo)
+                if given_date < min_date or now < given_date:
+                    error_message = "Invalid date selected."
+            except ValueError:
+                error_message = "Invalid date selected."
+        message = "Choose a date between {} and {} to view the chat archive:".format(min_date.strftime('%b-%d-%Y'), now.strftime('%b-%d-%Y'))
+        if error_message != None:
+            return render(request, self.template_name, {'topic' : topic, 'error_message' : error_message, 'message' : message})
+        chat_messages = ChatMessage.objects.filter(created__gte=given_date).filter(created__lte=given_date + datetime.timedelta(days=1))
+        #format date
+        given_date = given_date.strftime('%b-%d-%Y')
+        return render(request, self.template_name, {'topic' : topic, 'chat_messages' : chat_messages, 'date' : given_date, 'error_message' : error_message, 'message' : message})
