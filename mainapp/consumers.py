e7dae47ef1b28f1321bf7f0e9fb1409da6573536
Plogging Dev <ploggingdev@gmail.com>
2018-02-10 22:40:16 -0800

    Refactor ChatConsumer

--- mainapp/consumers.py
+++ mainapp/consumers.py
@@ -17,19 +17,6 @@ from hackerschat import celery
 
 class ChatConsumer(JsonWebsocketConsumer):
 
-    def group_message(self, event):
-        self.send_json({
-            'user' : event['user'],
-            'message' : event['message'],
-            'user_profile_url' : event['user_profile_url']
-        })
-    
-    def presence_data(self, event):
-        self.send_json({
-            'message_type': 'presence',
-            'payload': event['payload']
-        })
-
     def connect(self):
         topic_name = self.scope['url_route']['kwargs']['topic_name']
         try:
@@ -90,62 +77,89 @@ class ChatConsumer(JsonWebsocketConsumer):
         last_message_id = content.get('last_message_id', None)
         if message == None and heartbeat == None and last_message_id == None:
             return
-        #check for hearbeat
+        #heartbeat
         if heartbeat:
-            user_info = {'last_seen' : time.time(), 'reply_channel' : self.channel_name, 'topic' : topic_name}
-            #add to redis
-            if self.scope["user"].is_authenticated:
-                user_info['username'] = self.scope["user"].username
-            else:
-                user_info['username'] = None
-            user_list = cache.get('user_list')
-            if user_list != None:
-                user_updated = False
-                for item in user_list:
-                    if item['reply_channel'] == user_info['reply_channel']:
-                        user_list.remove(item)
-                        item['last_seen'] = time.time()
-                        user_list.append(item)
-                        cache.set('user_list', user_list)
-                        user_updated = True
-                if not user_updated:
-                    user_list.append(user_info)
-                    cache.set('user_list', user_list)
+            self.heartbeat(topic_name)
             return
         #scrollback
         if last_message_id:
-            chat_queryset = ChatMessage.objects.filter(topic=topic).filter(id__lte=last_message_id).order_by("-created")[:10]
-            chat_message_count = len(chat_queryset)
-            if chat_message_count > 0:
-                first_message_id = chat_queryset[len(chat_queryset)-1].id
-            else:
-                first_message_id = -1
-            previous_id = -1
-            if first_message_id != -1:
-                try:
-                    previous_id = ChatMessage.objects.filter(topic=topic).filter(pk__lt=first_message_id).order_by("-pk")[:1][0].id
-                except IndexError:
-                    previous_id = -1
-
-            chat_messages = reversed(chat_queryset)
-            cleaned_chat_messages = list()
-            for item in chat_messages:
-                current_message = item.message_html
-                user_profile_url = reverse('user_auth:public_user_profile', args=[item.user.username])
-                cleaned_item = {'user' : item.user.username, 'message' : current_message, 'user_profile_url' : user_profile_url}
-                cleaned_chat_messages.append(cleaned_item)
-
-            my_dict = { 'messages' : cleaned_chat_messages, 'previous_id' : previous_id }
-            self.send_json(
-                {
-                    'message_type' : 'scrollback',
-                    'messages' : cleaned_chat_messages,
-                    'previous_id' : previous_id,
-                },
-            )
+            self.scrollback(topic, last_message_id)
             return
+        #chat message
+        self.handle_chat_message(message, topic)
+
+    def disconnect(self, code):
+        topic_name = self.scope['url_route']['kwargs']['topic_name']
+        try:
+            topic = Topic.objects.get(name=topic_name)
+        except ObjectDoesNotExist:
+            return
+        async_to_sync(self.channel_layer.group_discard)(topic_name, self.channel_name)
+        user_info = {'last_seen' : time.time(), 'reply_channel' : self.channel_name, 'topic' : topic_name}
+        #remove from redis
+        user_list = cache.get('user_list')
+        if user_list != None:
+            for item in user_list:
+                if item['reply_channel'] == user_info['reply_channel']:
+                    user_list.remove(item)
+            cache.set('user_list', user_list)
+    
+    def heartbeat(self, topic_name):
+        user_info = {'last_seen' : time.time(), 'reply_channel' : self.channel_name, 'topic' : topic_name}
+        #add to redis
+        if self.scope["user"].is_authenticated:
+            user_info['username'] = self.scope["user"].username
+        else:
+            user_info['username'] = None
+        user_list = cache.get('user_list')
+        if user_list != None:
+            user_updated = False
+            for item in user_list:
+                if item['reply_channel'] == user_info['reply_channel']:
+                    user_list.remove(item)
+                    item['last_seen'] = time.time()
+                    user_list.append(item)
+                    cache.set('user_list', user_list)
+                    user_updated = True
+            if not user_updated:
+                user_list.append(user_info)
+                cache.set('user_list', user_list)
+    
+    def scrollback(self, topic, last_message_id):
+        chat_queryset = ChatMessage.objects.filter(topic=topic).filter(id__lte=last_message_id).order_by("-created")[:10]
+        chat_message_count = len(chat_queryset)
+        if chat_message_count > 0:
+            first_message_id = chat_queryset[len(chat_queryset)-1].id
+        else:
+            first_message_id = -1
+        previous_id = -1
+        if first_message_id != -1:
+            try:
+                previous_id = ChatMessage.objects.filter(topic=topic).filter(pk__lt=first_message_id).order_by("-pk")[:1][0].id
+            except IndexError:
+                previous_id = -1
+
+        chat_messages = reversed(chat_queryset)
+        cleaned_chat_messages = list()
+        for item in chat_messages:
+            current_message = item.message_html
+            user_profile_url = reverse('user_auth:public_user_profile', args=[item.user.username])
+            cleaned_item = {'user' : item.user.username, 'message' : current_message, 'user_profile_url' : user_profile_url}
+            cleaned_chat_messages.append(cleaned_item)
+
+        my_dict = { 'messages' : cleaned_chat_messages, 'previous_id' : previous_id }
+        self.send_json(
+            {
+                'message_type' : 'scrollback',
+                'messages' : cleaned_chat_messages,
+                'previous_id' : previous_id,
+            },
+        )
+    
+    def handle_chat_message(self, message, topic):
+        topic_name = topic.name
         #ignore message if user is not authenticated
-        if not self.scope["user"].is_authenticated:
+        if not self.scope["user"].is_authenticated or not message:
             return
         #check if rate limit has been reached
         if ChatMessage.objects.filter(user=self.scope["user"]).filter(created__gte=timezone.now() - datetime.timedelta(minutes=1)).count() >= 10:
@@ -155,9 +169,6 @@ class ChatConsumer(JsonWebsocketConsumer):
                         'message': "Rate limit reached. You can send 10 messages per minute.",
                     }
             })
-            return
-
-        if not message:
             return
         
         current_message = escape(message)
@@ -194,19 +205,16 @@ class ChatConsumer(JsonWebsocketConsumer):
                 'user_profile_url' : user_profile_url
             },
         )
-
-    def disconnect(self, code):
-        topic_name = self.scope['url_route']['kwargs']['topic_name']
-        try:
-            topic = Topic.objects.get(name=topic_name)
-        except ObjectDoesNotExist:
-            return
-        async_to_sync(self.channel_layer.group_discard)(topic_name, self.channel_name)
-        user_info = {'last_seen' : time.time(), 'reply_channel' : self.channel_name, 'topic' : topic_name}
-        #remove from redis
-        user_list = cache.get('user_list')
-        if user_list != None:
-            for item in user_list:
-                if item['reply_channel'] == user_info['reply_channel']:
-                    user_list.remove(item)
-            cache.set('user_list', user_list)
+    
+    def group_message(self, event):
+        self.send_json({
+            'user' : event['user'],
+            'message' : event['message'],
+            'user_profile_url' : event['user_profile_url']
+        })
+    
+    def presence_data(self, event):
+        self.send_json({
+            'message_type': 'presence',
+            'payload': event['payload']
+        })
